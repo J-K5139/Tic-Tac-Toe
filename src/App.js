c0e2748ae8403b26286479307d368bae77930b20
Jitesh Pentakota <pentakota_jitesh.ghrcecs@raisoni.net>
2021-06-18 14:00:43 +0530

    added helper.js and calculated the winner and displayed it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Board from './components/Board';
 import './styles/root.scss';
+import {calculateWinner} from "./helpers";
+import Square from "./components/Square";
 
 const App = () => {
-  return (
+
+    const [board, setboard] = useState(Array(9).fill(null));
+    console.log(board);
+    const [isXNext, setisXNext] = useState(false);
+
+    const winner = calculateWinner(board);
+    console.log(winner);
+    const declare = winner ? `Winner is ${winner}` : `Next player is ${isXNext ? 'X' : 'O' }`;
+
+    const handleSquareClick = position => {
+        if (board[position] || winner!==null) {
+            return;
+        }
+
+        setboard(prev => {
+            console.log(position);
+            return prev.map((square, pos) => {
+                if (pos === position) {
+                    return isXNext ? 'X' : 'O';
+                }
+                return square;
+            });
+        });
+        setisXNext(prev => !prev);
+    };
+
+
+                return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
-      <Board />
+        <h3>{declare}</h3>
+      <Board board={board} handleSquareClick={handleSquareClick}/>
     </div>
   );
 };
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,40 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Square from './Square';
 
-const Board = () => {
-  const [board, setboard] = useState(Array(9).fill(null));
-  console.log(board);
-  const [isXNext,setisXNext]= useState(false);
-
-  const handleSquareClick = (position) => {
-
-      if(board[position]){
-          return;
-      }
-
-    setboard(prev => {
-        console.log(position);
-      return prev.map((square, pos) => {
-        if (pos === position) {
-          return isXNext ? 'X' : 'O';
-        }
-
-        return square;
-
-      });
-    });
-    setisXNext((prev)=>(!prev));
-  };
+const Board = ({board,handleSquareClick}) => {
 
   const renderSquare = position => {
-      console.log(position);
+    console.log(position);
     return (
-      <Square
-        value={board[position]}
-        onClick={() => handleSquareClick(position)}
-      />
+        <Square
+            value={board[position]}
+            onClick={() => handleSquareClick(position)}
+        />
     );
   };
+
 
   return (
     <div className="board">
